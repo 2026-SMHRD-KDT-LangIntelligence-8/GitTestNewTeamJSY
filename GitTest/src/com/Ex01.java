5d0e460d1f4898f19f2b0cefedb5a9cd61d406ab
PJH <wnsgud75a@gmail.com>
2026-02-25 16:44:22 +0900

    준형 개인 브렌치 커밋

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -11,6 +11,8 @@ public class Ex01 {
 		System.out.println("팀원 1 여섯번째 커밋");
 		System.out.println("팀원 3 일곱번째 커밋");
 		System.out.println("팀원 2 여덟번째 커밋");
+		
+		System.out.println("준형 개인 브렌치 커밋");
 	}
 
 }
