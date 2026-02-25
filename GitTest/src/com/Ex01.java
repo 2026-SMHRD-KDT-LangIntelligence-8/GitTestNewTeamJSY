fb79c0cee115fc3bf2725a0be1db7be8ae535d60
KHH <kt41337995@gmail.com>
2026-02-25 15:55:12 +0900

    팀원 3 김홍현 여덟번째 커밋

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -10,6 +10,7 @@ public class Ex01 {
 		System.out.println("팀장 4 다섯번째 커밋");
 		System.out.println("팀원 1 여섯번째 커밋");
 		System.out.println("팀원 3 일곱번째 커밋");
+		System.out.println("팀원 2 여덟번째 커밋");
 	}
 
 }
